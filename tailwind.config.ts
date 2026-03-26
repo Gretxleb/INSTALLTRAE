9c9d692f3bbc7ce111f0da43bc5129a151ccc0e3
Sengel Paul <stasyaklose@gmail.com>
2026-03-26 21:24:07 +0100

    initial portfolio

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#000000",
+        surface: "#111111",
+        accent: {
+          purple: "#A855F7",
+          blue: "#3B82F6",
+          neon: {
+            purple: "#BF40BF",
+            blue: "#00FFFF",
+          }
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
